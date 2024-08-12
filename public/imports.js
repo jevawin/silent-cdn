d18124c42d40682a726f82d5cd218b6735112e87
Jamie Evawin <iamjevawin@gmail.com>
2024-08-12 10:53:50 +0100

    Imports JS for head

---
+++ public/imports.js
@@ -0,0 +1,30 @@
+(async () => {
+  // import path-specific script
+  try {
+    const url = `https://silent-cdn.pages.dev${document.location.pathname}/index.js`;
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const scriptTag = document.createElement("script");
+    scriptTag.src = url;
+    document.head.appendChild(scriptTag);
+  } catch (error) {
+    console.error(error.message);
+  }
+
+  // import path-specific stylesheet
+  try {
+    const url = `https://silent-cdn.pages.dev${document.location.pathname}/index.css`;
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const styleTag = document.createElement("link");
+    styleTag.rel = "stylesheet";
+    styleTag.href = url;
+    document.head.appendChild(styleTag);
+  } catch (error) {
+    console.error(error.message);
+  }
+})();
