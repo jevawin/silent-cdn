5c4bdb4d27be5cb07ee4d993da66d9e9893e8e97
Jamie Evawin <iamjevawin@gmail.com>
2024-09-29 10:25:58 +0100

    Full local dev with importer

--- public/imports.js
+++ public/imports.js
@@ -4,45 +4,7 @@
 // local dev
 const host = window.localStorage.getItem("js_host") || "https://silent-cdn.pages.dev";
 
-// newrelic error monitoring
-const nrScript = document.createElement("script");
-let nrError = null;
-nrScript.src = `${host}/newrelic.js`;
-nrScript.onload = () => {
-  nrError = (error) => {
-    newrelic.noticeError(new Error(error));
-  };
-};
-document.head.appendChild(nrScript);
-
-(async () => {
-  // import path-specific script
-  try {
-    const url = `${host}${document.location.pathname}/index.js`;
-
-    const response = await fetch(url);
-    if (!response.ok) {
-      nrError(response.status);
-    }
-    const scriptTag = document.createElement("script");
-    scriptTag.src = url;
-    document.head.appendChild(scriptTag);
-  } catch (error) {
-    console.log(error.message);
-  }
-
-  // import path-specific stylesheet
-  try {
-    const url = `${host}${document.location.pathname}/index.css`;
-    const response = await fetch(url);
-    if (!response.ok) {
-      nrError(response.status);
-    }
-    const styleTag = document.createElement("link");
-    styleTag.rel = "stylesheet";
-    styleTag.href = url;
-    document.head.appendChild(styleTag);
-  } catch (error) {
-    nrError(error.message);
-  }
-})();
+// importer
+const importer = document.createElement("script");
+importer.src = `${host}/importer.js`;
+document.head.appendChild(importer);
